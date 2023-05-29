6f602db7b4dcd79b8b4ec700fab41d93b456fa1e
lawyerWho <134937174+lawyerWho@users.noreply.github.com>
2023-05-29 17:51:51 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// var React = require("react");
+// var ReactDOM = require("react-dom");
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+    <p>This is a paragraph.</p>
+  </div>,
+  document.getElementById("root")
+);
